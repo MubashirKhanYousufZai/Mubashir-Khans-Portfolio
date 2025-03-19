1bd21cbd45444dfecd63d4e0a3d61521616f6961
Mubashir Khan <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2025-03-19 06:46:52 +0500

    first commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
     { name: "About", path: "/components/hero" },
     { name: "Projects", path: "/components/projects" },
     { name: "Skills", path: "/components/skills" },
-    { name: "Experience", path: "/components/experience" },
+    { name: "Experience", path: "/components/experiance" },
     { name: "Contact", path: "/components/contact" },
   ];
 
